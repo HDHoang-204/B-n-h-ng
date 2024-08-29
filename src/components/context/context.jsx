82158aed005cce041da340c412f434f7e699253c
HDHoang-204 <hoanghdpd08497@fpt.edu.vn>
2024-08-30 01:07:57 +0700

    UPLOAD 4

--- src/components/context/context.jsx
+++ src/components/context/context.jsx
@@ -2,82 +2,83 @@ import "./context.scss"
 
 function Context() {
     return <div className="main">
-        <div className="main_top">
-            <div className="icon">Icon</div>
-            <div className="text">Blog</div>
+        <div className="top">
+            <div className="main_top">
+                <div className="icon">Icon</div>
+                <div className="text">Blog</div>
+            </div>
+
+            <div className="main_bot">
+                <div className="blog">
+                    <div className="image">
+                        <img src="https://th.bing.com/th/id/R.6ba1315b7ab65c1f9b4273df1d46a911?rik=HJI2pqJmAiS8BA&pid=ImgRaw&r=0" alt="" />
+                    </div>
+
+                    <div className="blog_text">
+                        <button>Lợi ích</button>
+                        <p>
+                            <a href="https://vnexpress.net/10-loi-ich-khi-an-sang-moi-ngay-4736666.html">Những lợi ích khi ăn sáng mỗi ngày</a>
+                        </p>
+                        <a href="https://vnexpress.net/10-loi-ich-khi-an-sang-moi-ngay-4736666.html">Xem chi tiết</a>
+                    </div>
+                </div>
+
+                <div className="blog">
+                    <div className="image">
+                        <img src="https://th.bing.com/th/id/R.ba9754aac38ff60183aa12a21beaa0e1?rik=A%2b2PzGbYNMTWjg&riu=http%3a%2f%2fgiavihanhphuc.com%2ffiles%2ffolder_1401%2fimages%2fmi-quang-ga-8Wm000.jpg&ehk=Urytr9n%2fg30EJBKBISFu1Yki1J0k9nEZ6F6NRJw%2fw3A%3d&risl=&pid=ImgRaw&r=0" alt="" />
+                    </div>
+
+                    <div className="blog_text">
+                        <button>Hướng dẫn</button>
+                        <p>
+                            <a href="https://www.bachhoaxanh.com/kinh-nghiem-hay/them-an-mi-quang-day-la-cach-nau-mi-quang-dung-vi-ma-khong-can-ra-tiem-1153609">Cách nấu mì Quảng đúng vị mà không cần ra tiệm</a>
+                        </p>
+                        <a href="https://www.bachhoaxanh.com/kinh-nghiem-hay/them-an-mi-quang-day-la-cach-nau-mi-quang-dung-vi-ma-khong-can-ra-tiem-1153609s">Xem chi tiết</a>
+                    </div>
+                </div>
+
+                <div className="blog">
+                    <div className="image">
+                        <img src="https://th.bing.com/th/id/OIP.1_Py6gY6iYrEEr6JjSy2pQAAAA?rs=1&pid=ImgDetMain" alt="" />
+                    </div>
+
+                    <div className="blog_text">
+                        <button>Hướng dẫn</button>
+                        <p>
+                            <a href="https://www.bachhoaxanh.com/kinh-nghiem-hay/10-cach-lam-com-chien-don-gian-tai-nha-thom-ngon-gion-toi-1418849">10 cách làm cơm chiên đơn giản tại nhà thơm ngon giòn tơi</a>
+                        </p>
+                        <a href="https://www.bachhoaxanh.com/kinh-nghiem-hay/10-cach-lam-com-chien-don-gian-tai-nha-thom-ngon-gion-toi-1418849">Xem chi tiết</a>
+                    </div>
+                </div>
+
+                <div className="blog">
+                    <div className="image">
+                        <img src="https://th.bing.com/th/id/R.fd80059a45fdaa1e86f23c48836d6bfe?rik=ux5DoD0yamXSJA&riu=http%3a%2f%2fnuc.ibcinstitute.com%2fibc2017%2fwp-content%2fuploads%2f2019%2f09%2fUntitled-1-1024x731.png&ehk=B73bEiFj1DlyhnrR0xnjepcfdrhzTpc1atLcn%2fmaoXE%3d&risl=&pid=ImgRaw&r=0" alt="" />
+                    </div>
+
+                    <div className="blog_text">
+                        <button>Lợi ích</button>
+                        <p>
+                            <a href="https://kduoc.com/ban-tin-suc-khoe/nhung-loi-ich-khi-an-trung-thuong-xuyen/#:~:text=Nh%E1%BB%AFng%20l%E1%BB%A3i%20%C3%ADch%20khi%20%C4%83n%20tr%E1%BB%A9ng%20th%C6%B0%E1%BB%9Dng%20xuy%C3%AAn,...%206%20Gi%E1%BA%A3m%20c%C3%A2n%20d%E1%BB%85%20d%C3%A0ng%20h%C6%A1n%20">Những lợi ích khi ăn trứng thường xuyên</a>
+                        </p>
+                        <a href="https://kduoc.com/ban-tin-suc-khoe/nhung-loi-ich-khi-an-trung-thuong-xuyen/#:~:text=Nh%E1%BB%AFng%20l%E1%BB%A3i%20%C3%ADch%20khi%20%C4%83n%20tr%E1%BB%A9ng%20th%C6%B0%E1%BB%9Dng%20xuy%C3%AAn,...%206%20Gi%E1%BA%A3m%20c%C3%A2n%20d%E1%BB%85%20d%C3%A0ng%20h%C6%A1n%20">Xem chi tiết</a>
+                    </div>
+                </div>
+
+
+            </div>
         </div>
-
-        <div className="main_bot">
-            <div className="blog">
-                <div className="image">
-                    <img src="https://th.bing.com/th/id/R.6ba1315b7ab65c1f9b4273df1d46a911?rik=HJI2pqJmAiS8BA&pid=ImgRaw&r=0" alt="" />
-                </div>
-
-                <div className="blog_text">
-                    <button>Lợi ích</button>
-                    <p>
-                        <a href="https://vnexpress.net/10-loi-ich-khi-an-sang-moi-ngay-4736666.html">Những lợi ích khi ăn sáng mỗi ngày</a>
-                    </p>
-                    <a href="https://vnexpress.net/10-loi-ich-khi-an-sang-moi-ngay-4736666.html">Xem chi tiết</a>
-                </div>
-            </div>
-
-            <div className="blog">
-                <div className="image">
-                    <img src="https://th.bing.com/th/id/R.ba9754aac38ff60183aa12a21beaa0e1?rik=A%2b2PzGbYNMTWjg&riu=http%3a%2f%2fgiavihanhphuc.com%2ffiles%2ffolder_1401%2fimages%2fmi-quang-ga-8Wm000.jpg&ehk=Urytr9n%2fg30EJBKBISFu1Yki1J0k9nEZ6F6NRJw%2fw3A%3d&risl=&pid=ImgRaw&r=0" alt="" />
-                </div>
-
-                <div className="blog_text">
-                    <button>Hướng dẫn</button>
-                    <p>
-                        <a href="https://www.bachhoaxanh.com/kinh-nghiem-hay/them-an-mi-quang-day-la-cach-nau-mi-quang-dung-vi-ma-khong-can-ra-tiem-1153609">Cách nấu mì Quảng đúng vị mà không cần ra tiệm</a>
-                    </p>
-                    <a href="https://www.bachhoaxanh.com/kinh-nghiem-hay/them-an-mi-quang-day-la-cach-nau-mi-quang-dung-vi-ma-khong-can-ra-tiem-1153609s">Xem chi tiết</a>
-                </div>
-            </div>
-
-            <div className="blog">
-                <div className="image">
-                    <img src="https://th.bing.com/th/id/OIP.1_Py6gY6iYrEEr6JjSy2pQAAAA?rs=1&pid=ImgDetMain" alt="" />
-                </div>
-
-                <div className="blog_text">
-                    <button>Hướng dẫn</button>
-                    <p>
-                        <a href="https://www.bachhoaxanh.com/kinh-nghiem-hay/10-cach-lam-com-chien-don-gian-tai-nha-thom-ngon-gion-toi-1418849">10 cách làm cơm chiên đơn giản tại nhà thơm ngon giòn tơi</a>
-                    </p>
-                    <a href="https://www.bachhoaxanh.com/kinh-nghiem-hay/10-cach-lam-com-chien-don-gian-tai-nha-thom-ngon-gion-toi-1418849">Xem chi tiết</a>
-                </div>
-            </div>
-
-            <div className="blog">
-                <div className="image">
-                    <img src="https://th.bing.com/th/id/R.fd80059a45fdaa1e86f23c48836d6bfe?rik=ux5DoD0yamXSJA&riu=http%3a%2f%2fnuc.ibcinstitute.com%2fibc2017%2fwp-content%2fuploads%2f2019%2f09%2fUntitled-1-1024x731.png&ehk=B73bEiFj1DlyhnrR0xnjepcfdrhzTpc1atLcn%2fmaoXE%3d&risl=&pid=ImgRaw&r=0" alt="" />
-                </div>
-
-                <div className="blog_text">
-                    <button>Lợi ích</button>
-                    <p>
-                        <a href="https://kduoc.com/ban-tin-suc-khoe/nhung-loi-ich-khi-an-trung-thuong-xuyen/#:~:text=Nh%E1%BB%AFng%20l%E1%BB%A3i%20%C3%ADch%20khi%20%C4%83n%20tr%E1%BB%A9ng%20th%C6%B0%E1%BB%9Dng%20xuy%C3%AAn,...%206%20Gi%E1%BA%A3m%20c%C3%A2n%20d%E1%BB%85%20d%C3%A0ng%20h%C6%A1n%20">Những lợi ích khi ăn trứng thường xuyên</a>
-                    </p>
-                    <a href="https://kduoc.com/ban-tin-suc-khoe/nhung-loi-ich-khi-an-trung-thuong-xuyen/#:~:text=Nh%E1%BB%AFng%20l%E1%BB%A3i%20%C3%ADch%20khi%20%C4%83n%20tr%E1%BB%A9ng%20th%C6%B0%E1%BB%9Dng%20xuy%C3%AAn,...%206%20Gi%E1%BA%A3m%20c%C3%A2n%20d%E1%BB%85%20d%C3%A0ng%20h%C6%A1n%20">Xem chi tiết</a>
-                </div>
-            </div>
-
-
-        </div>
-
 
         <div className="main2">
             <div className="main2_top">
                 <div className="main2_about">
                     <div className="image2">
-                        <img src="" alt="" />
+                        <img src="https://bizweb.dktcdn.net/100/332/765/themes/894765/assets/banner_about.png?1676258982209" alt="" />
                     </div>
-                    <div className="">
+                    <div className="texttt">
                         <div className="text1">
                             <h3>TOMATO</h3>
-                            <p></p>
+                            <p>Được khởi xướng và vận hành bởi một nhóm người trẻ năng động, cửa hàng bánh ngọt và đồ uống DELICIOUS đang dần khẳng định chỗ đứng trong thị trường đồ ăn giải khát phong phú và đa dạng ở Hà Nội. Hướng tới đối tượng khách hàng là các bạn trẻ năng động, chúng tôi quan tâm tới chất lượng và hình ảnh của sản phẩm</p>
                             <a href="">Đọc thêm</a>
                         </div>
                         <p>Kết nối với chung tôi : </p>
@@ -90,12 +91,35 @@ function Context() {
 
 
             <div className="main2_bot">
-                <div className="icom">
-                    ICON
+                <div className="main2_bot2">
+                    <div className="icom">
+                        <img src="https://bizweb.dktcdn.net/100/332/765/themes/894765/assets/service_1.png?1676258982209" alt="" />
+                    </div>
+                    <div className="main2_text">
+                        <h3>Chuyển phát nhanh</h3>
+                        <p>Chúng tôi miễn phí với đơn hàng trên 350.000đ tại Hà Nội và free ship toàn quốc với đơn hàng 700.000đ</p>
+                    </div>
                 </div>
-                <div className="main2_text">
-                    <h3>Chuyển phát nhanh</h3>
-                    <p>Chúng tôi miễn phí với đơn hàng trên 350.000đ tại Hà Nội và free ship toàn quốc với đơn hàng 700.000đ</p>
+
+                <div className="main2_bot2">
+                    <div className="icom">
+                        <img src="https://bizweb.dktcdn.net/100/332/765/themes/894765/assets/service_1.png?1676258982209" alt="" />
+                    </div>
+                    <div className="main2_text">
+                        <h3>Đặt bàn </h3>
+                        <p>Với ToMaTo, bạn có thể đặt chỗ ngay tại nhà mà không cần phải gọi điện, đồng thời nhận các ưu đãi giảm giá khi đặt trước, giúp tiết kiệm chi phí hiệu quả</p>
+                    </div>
+                </div>
+
+
+                <div className="main2_bot2">
+                    <div className="icom">
+                        <img src="https://bizweb.dktcdn.net/100/332/765/themes/894765/assets/service_3.png?1676258982209" alt="" />
+                    </div>
+                    <div className="main2_text">
+                        <h3>Hỗ trợ khách hàng</h3>
+                        <p>Trung tâm chăm sóc khách hàng của ToMaTo, hotline 1900 6750 để được hỗ trợ giải đáp</p>
+                    </div>
                 </div>
             </div>
         </div>
--- src/pages/sigin/sigin.jsx
+++ src/pages/sigin/sigin.jsx
@@ -1,7 +1,37 @@
+import "./sigi.scss"
+
 function Sigin() {
     return <>
+        <div class="grid">
 
-        <h1>Sigin</h1>
+            <form action="/" method="post" class="form login">
+
+                <header class="login__header">
+                    <h3 class="login__title">Login</h3>
+                </header>
+
+                <div class="login__body">
+
+                    <div class="form__field">
+                        <input type="email" placeholder="Email" required />
+                    </div>
+
+                    <div class="form__field">
+                        <input type="password" placeholder="Password" required />
+                    </div>
+
+                </div>
+
+                <footer class="login__footer">
+                    <input type="submit" value="Login" />
+
+                    <p><span class="icon icon--info">?</span><a href="#">Forgot Password</a></p>
+                </footer>
+
+            </form>
+
+        </div>
+
     </>
 }
 
